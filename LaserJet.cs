3752d36539f4e07a7dba79f35584f44e8a4014ec
Lincchi <caroline.lincchi@gmail.com>
2020-05-05 10:46:46 +0700

    Tugas Polymorphism

---
+++ LaserJet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Printer
+{
+    public class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12");
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("LaserJet printer printing ....");
+        }
+    }
+}
---
+++ PrinterWindows.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Printer
+{
+    public class PrinterWindows
+    {
+        public string Name { get; set; }
+        public virtual void Show()
+        {
+            Console.WriteLine("Printer display dimension : ....");
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Printer is printing ....");
+        }
+    }
+}
